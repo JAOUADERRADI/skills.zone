026a08a7af49433b28d17add9e268ffc9a30f155
jaouad erradi <jaouaderradi@proton.me>
2024-09-26 13:50:38 +0200

    Implement CRUD for course post managment

---
+++ src/Controller/Admin/PostCategoryController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\PostCategory;
+use App\Form\PostCategoryType;
+use App\Repository\PostCategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('admin/post/category')]
+final class PostCategoryController extends AbstractController
+{
+    #[Route(name: 'app_post_category_index', methods: ['GET'])]
+    public function index(PostCategoryRepository $postCategoryRepository): Response
+    {
+        return $this->render('Admin/post_category/index.html.twig', [
+            'post_categories' => $postCategoryRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_post_category_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $postCategory = new PostCategory();
+        $form = $this->createForm(PostCategoryType::class, $postCategory);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($postCategory);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_post_category_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('Admin/post_category/new.html.twig', [
+            'post_category' => $postCategory,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_post_category_show', methods: ['GET'])]
+    public function show(PostCategory $postCategory): Response
+    {
+        return $this->render('Admin/post_category/show.html.twig', [
+            'post_category' => $postCategory,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_post_category_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, PostCategory $postCategory, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PostCategoryType::class, $postCategory);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_post_category_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('Admin/post_category/edit.html.twig', [
+            'post_category' => $postCategory,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_post_category_delete', methods: ['POST'])]
+    public function delete(Request $request, PostCategory $postCategory, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$postCategory->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($postCategory);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_post_category_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
