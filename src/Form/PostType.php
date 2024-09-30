65bc295e7ebb23bbda5c999a861dc03fcb06be63
jaouad erradi <jaouaderradi@proton.me>
2024-09-30 09:39:49 +0200

    Add validation constraints for Post form fields

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -12,21 +12,51 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('description')
-            ->add('image')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
+            ->add('title', TextType::class, [
+                'label' => 'Title',
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length([
+                        'max' => 225
+                    ]),
+                ],
             ])
-            ->add('updatedAt', null, [
-                'widget' => 'single_text',
+            ->add('content', TextareaType::class, [
+                'label' => 'Content',
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'The content should not be blank.',
+                    ]),
+                ],
+            ])
+            ->add('description', TextareaType::class, ([
+                'label' => 'Short Description',
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'The description should not be blank.',
+                    ]),
+                    new Assert\Length(['max' => 500]),
+                ],
+            ]))
+            ->add('image', FileType::class, [
+                'label' => 'Upload Image',
+                'required' => false,
+                'constraints' => [
+                    new Assert\Image([
+                        'maxSize' => '10M',
+                    ]),
+                ]
             ])
             ->add('status', ChoiceType::class, [
                 'choices' => [
@@ -34,10 +64,6 @@ class PostType extends AbstractType
                     'Published' => PostStatus::Published,
                     'Archived' => PostStatus::Archived,
                 ],
-            ])
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
             ])
             ->add('category', EntityType::class, [
                 'class' => PostCategory::class,
