4999fcd3ea749e1750b0fdd72c2d96b6b84e375f
jaouad erradi <jaouaderradi@proton.me>
2024-10-02 10:04:44 +0200

    Add pagination to and display posts (resources) in PostController

--- src/Controller/Frontend/PostController.php
+++ src/Controller/Frontend/PostController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller\Frontend;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 
 class PostController extends AbstractController
 {
-    #[Route('/post', name: 'app_post')]
-    public function index(): Response
+    #[Route('/resources', name: 'app_post')]
+    public function index(EntityManagerInterface $entityManager, PaginatorInterface $paginator, Request $request): Response
     {
+        $postsQuery = $entityManager->getRepository(Post::class)->createQueryBuilder('p')->getQuery();
+        $posts = $paginator->paginate(
+            $postsQuery,
+            $request->query->getInt('page', 1),
+            9
+        );
         return $this->render('Frontend/post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $posts,
         ]);
     }
 }
