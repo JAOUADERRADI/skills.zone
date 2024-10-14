0e8703b2e0adcc1da0912bc00ed8641d8dd6eb28
jaouad erradi <jaouaderradi@proton.me>
2024-10-14 15:08:11 +0200

    Add info user

---
+++ src/Form/InfoUserType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\InfoUser;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InfoUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('FirstName')
+            ->add('LastName')
+            ->add('phone')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => InfoUser::class,
+        ]);
+    }
+}
