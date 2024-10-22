01e2465566520c876adb8d3d2c9fb0bf8c207614
jaouad erradi <jaouaderradi@proton.me>
2024-10-22 11:49:10 +0200

    Add animation for menu icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const mobileMenuButton = document.getElementById("mobile-menu-button");
   const mobileNav = document.getElementById("mobile-nav");
+  const menuIcon = document.getElementById("menu-icon");
 
   mobileMenuButton.addEventListener("click", function () {
     mobileNav.classList.toggle("mobile-nav--active");
+    menuIcon.classList.toggle("menu-open");
   });
 
   const profileButton = document.getElementById("profile-menu-button");
